d6f6271f4b6ee88180edccc1817888a11a4077ba
Kushagra Srivastava <kushagra8858@gmail.com>
2023-01-31 12:24:26 +0530

    input

---
+++ area.py
@@ -0,0 +1,6 @@
+# input('prompt')
+r = int(input("enter radius of circle:👍"))
+print(f'Radius of circle is {r}')
+area = 22/7 * r ** 2
+print(f'Radius of cicle is {r}')
+print(f'Area of circle is {area}')
